c0763376e21782bbb6fcf4529974d2cfedf8ba02
Adriana Harseva <a.harseva@gmail.com>
2022-10-15 12:43:31 +0100

    task manager done - not styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # As a user, I can list tasks (read)
+  get 'tasks', to: 'tasks#index'
+  # As a user, I can add a new task (create)
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # As a user, I can view the details of a task (read)
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # As a user, I can edit a task (mark as completed / update title & details) (edit)
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  # As a user, I can remove a task (delete)
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
